3aa127fc88957e1b996afdf563a245b5af70f2f1
HamiltonOchoa17 <hamiltonochoa@gmail.com>
2022-06-23 14:41:21 -0500

    Actualizacion Menu

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const btnMenu = document.querySelector("#btn-menu");
-const mainNav= document.querySelector("#main-nav");
-btnMenu.addEventListener("click", function(){
-    mainNav.classList.toggle("mostrar");
+const mainNav= document.querySelector(".menu");
+btnMenu.addEventListener("click", () => {
+    mainNav.classList.toggle("mostrar_menu");
 });
+
+
+
+
